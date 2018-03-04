63962af0584938e639d74613ac989b423e12dc8c
TristanEduProjet <TristanEduProjet@users.noreply.github.com>
2018-03-04 17:29:17 +0100

    Upgrade JUnit from v4 to v5

--- src/test/java/rpn/CLITest.java
+++ src/test/java/rpn/CLITest.java
@@ -1,28 +1,35 @@
 package rpn;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static rpn.CLI.evaluate;
 
+@DisplayName("Test CLI RPN")
 public class CLITest {
 
     @Test
+    @DisplayName("single number (single digit)")
     public void should_evaluate_single_digit_constant() {
         assertThat(evaluate("5")).isEqualTo(5);
     }
 
     @Test
+    @DisplayName("single number (multiple digits)")
     public void should_evaluate_multiple_digits_constant() {
         assertThat(evaluate("17")).isEqualTo(17);
     }
 
     @Test
+    @DisplayName("Simple addition")
     public void should_evaluate_simple_addition() {
         assertThat(evaluate("17 5 +")).isEqualTo(22);
     }
 
     @Test
+    @DisplayName("Two additions")
     public void should_evaluate_more_complex_addition() {
         assertThat(evaluate("2 3 5 + +")).isEqualTo(10);
     }
